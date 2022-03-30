e780d99017d9783c7c83ebb0fa06650a88100647
Gunther Klessinger <gunther.klessinger@axiros.com>
2022-03-30 11:06:42 +0200

    chore setup target

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ here="$(
 d_assets="$HOME/.local/share/nvim.assets"
 function set_symlinks {
 	S="$here"
-	T="$HOME/.config/nvim"
+	T="$d_astro"
 	rm -f "$T/lua/user"
 	ln -s "$S" "$T/lua/user"
 	for k in spell ftplugin snippets; do
@@ -14,27 +14,38 @@ function set_symlinks {
 		ln -s "$S/$k" "$T/$k"
 	done
 }
-function download_spell {
-	local fn="$here/spell/de.utf-8.spl"
-	test -e "$fn" && return
-	rm -f "$fn"
-	(cd "$d_assets" && wget 'http://ftp.vim.org/pub/vim/runtime/spell/de.utf-8.spl') || exit 1
-	ln -s "$d_assets/de.utf-8.spl" "$fn"
-}
-function download_10 {
-	local fn="$here/10k.txt"
-	test -e "$fn" && return
-	rm -f "$fn"
-	(cd "$d_assets" && wget "https://raw.githubusercontent.com/neoclide/coc-sources/master/packages/word/10k.txt") || exit 1
-	ln -s "$d_assets/10k.txt" "$fn"
-}
+# function download_spell {
+# 	local fn="$here/spell/de.utf-8.spl"
+# 	test -e "$fn" && return
+# 	rm -f "$fn"
+# 	(cd "$d_assets" && wget 'http://ftp.vim.org/pub/vim/runtime/spell/de.utf-8.spl') || exit 1
+# 	ln -s "$d_assets/de.utf-8.spl" "$fn"
+# }
+# function download_10 {
+# 	local fn="$here/10k.txt"
+# 	test -e "$fn" && return
+# 	rm -f "$fn"
+# 	(cd "$d_assets" && wget "https://raw.githubusercontent.com/neoclide/coc-sources/master/packages/word/10k.txt") || exit 1
+# 	ln -s "$d_assets/10k.txt" "$fn"
+# }
 
 function main {
+	set -x
 	test -n "$here" || exit 1
+	test -z "$1" && {
+		echo "require astrovim dir"
+		exit 1
+	}
+	d_astro="$(
+		builtin cd "$1" || exit 1
+		pwd
+	)" || exit 1
 	mkdir -p "$d_assets"
-	download_spell
-	download_10
+	#download_spell
+	#download_10
 	set_symlinks
+	set +x
+	echo "Done."
 }
 
 main "$@"
