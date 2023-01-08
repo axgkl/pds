17b3827f08a239c19c2c8d51b8336c74c35c33a6
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-01-08 21:20:20 +0100

    tests

--- test/test_pds_script.sh
+++ test/test_pds_script.sh
@@ -1,82 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
-
-out() {
-    local col="$1"
-    shift
-    local t="$1"
-    shift
-    echo -e "\x1b[1;48;5;${col};30m $t \x1b[1;48;5;255m $* \x1b[0m"
-}
-function die {
-    set +x
-    out 196 "Failed" "${cur_test:-}"
-    test -n "$1" && echo -e "$*"
-    exit 1
-}
-
-function pds { source "$HOME/.config/pds/setup/pds.sh" "$@"; }
-
-function tst {
-    cur_test="$1"
-    grep -q "$test_match" <<<"$cur_test" || {
-        out 125 "Skipped" "$cur_test"
-        return
-    }
-    out 119 "Test" "$*"
-    "$@" || exit 1
-
-    #     ("$@") && {
-    #         echo -e "✔️ $*"
-    #         return
-    #     } || true
-    #     out 125 "Error. Running again, traced: $*"
-    #     rerurn=true
-    #     "$@"
-    #     exit 1
-}
-
-function parse_args() {
-    cmd=()
-    asserts=''
-    assertmode=false
-    errmsg="$*"
-    while test -n "$1"; do
-        test "$1" == "|" && {
-            assertmode=true
-        }
-        test "$1" == "-" && {
-            shift
-            errmsg="$1"
-            break
-        }
-        $assertmode && asserts=''$asserts' '$1'' || cmd+=("$1")
-        shift
-    done
-}
-function testit {
-    echo -e "\x1b[37;2m${cmd[*]} $asserts\x1b[0m"
-    if [[ -n "$asserts" ]]; then
-        eval "${cmd[*]} $asserts"
-    else
-        "${cmd[@]}"
-    fi
-}
-function ✔️ {
-    parse_args "$@"
-    testit || die "Failed: $errmsg"
-}
-function ❌ {
-    parse_args "$@"
-    testit && die "Should have failed: $errmsg"
-    true
-}
-
+source test/tools.sh
 function bootstrap_nvim {
     cd
+    rm -rf pds.sh
     ✔️ wget https://raw.githubusercontent.com/AXGKl/pds/master/setup/pds.sh
     ✔️ chmod +x 'pds.sh'
     ✔️ ./pds.sh install
+}
+function bootstrap_nvim_again {
+    ❌ eval '( bootstrap_nvim >/dev/null )'
 }
 function pds_avail { ✔️ pds; }
 function vi_avail { ✔️ pds vi --version \| grep 'NVIM\ v'; }
@@ -106,22 +39,36 @@ function stash {
     ❌ test -d "$HOME/.local/state/nvim" - "nvim state dir still present"
     ❌ test -d "$HOME/.local/share/nvim" - "nvim share dir still present"
     ❌ pds stash mytest - "could restash with same name"
+}
+function clean_all {
+    ✔️ pds clean-all -f
+    ❌ test -d "$HOME/.config/nvim" - "nvim dir still present"
+    ❌ test -d "$HOME/.local/state/nvim" - "nvim state dir still present"
+    ❌ test -d "$HOME/.local/share/nvim" - "nvim share dir still present"
+}
+function restore {
+    local d="$HOME/.local/share/stashed_nvim"
     ✔️ pds restore mytest
     ✔️ test -e "$HOME/.config/nvim/init.lua"
     ✔️ test -e "$d/mytest/nvim/init.lua"
+    ✔️ pds status \| grep mytest - 'must be still available'
 }
+
 function main {
     # THESE MUST BE RUN IN ORDER.
     # You may comment out previous steps though, when testing locally.
     test_match="${1:-}"
     #
-    tst bootstrap_nvim
+    test -n "${skip_install:-}" || tst bootstrap_nvim
+    tst bootstrap_nvim_again
     tst pds_avail
     tst vi_avail
     tst pds_no_args
     tst help_short
     tst help_long
     tst stash
+    tst clean_all
+    tst restore
 
 }
 
