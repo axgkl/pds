a6e90e48de5a2ee4d7a734b3f251323080ef88b0
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-01-08 02:56:50 +0100

    tests: fix chmod

--- test/test_all.sh
+++ test/test_all.sh
@@ -4,7 +4,7 @@ set -e
 function test_bootstrap {
     cd
     wget https://raw.githubusercontent.com/AXGKl/pds/master/setup/pds.sh
-    chmod +x
+    chmod +x 'pds.sh'
     ./pds.sh
     ./pds.sh install
     export PATH="$HOME/pds/bin:$PATH"
