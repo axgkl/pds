8ddfefeae7c5c7caf3d20f4d85e4f7f60454d4ec
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-01-08 16:55:44 +0100

    tests: shell tests

--- test/test_pds_script.sh
+++ test/test_pds_script.sh
@@ -1,20 +1,42 @@
 #!/usr/bin/env bash
 set -e
-function run {
-    echo -e "\x1b[1;48;5;119mRunning: $*\x1b[0m"
-    ("$@")
+tst() {
+    echo -e "\x1b[1;48;5;119mRunning Test: $*\x1b[0m"
+    (source "$HOME/.bashrc" && "$@")
 }
-function test_bootstrap_nvim {
+function bootstrap_nvim {
     cd
     wget https://raw.githubusercontent.com/AXGKl/pds/master/setup/pds.sh
     chmod +x 'pds.sh'
-    ./pds.sh && ./pds.sh install
-    export PATH="$HOME/pds/bin:$PATH"
-    vi --version | grep NVIM
+    ./pds.sh install
+}
+function vi_avail {
+    pds vi --version | grep NXVIM
+}
+function pds_no_args {
+    pds | grep SWITCHES
+    pds | grep FUNCTIONS
+    pds | grep ACTIONS
+}
+function help_short {
+    pds -h | grep SWITCHES
+    pds -h | grep 'ACTION DETAILS'
+}
+function stash {
+    pds stash mytest
+    test -d "$HOME/.local/state"
+
 }
 
 function main {
-    run test_bootstrap_nvim
+    # THESE MUST BE RUN IN ORDER.
+    # You may comment out previous steps though, when testing locally.
+
+    tst bootstrap_nvim
+    tst vi_avail
+    tst pds_no_args
+    tst help_short
+    tst stash
 }
 
 main "$@"
