65bb5abeba017460995c50e448902b3729bcbf43
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-01-08 16:34:51 +0100

    tests: rename

--- test/test_pds_script.sh
+++ test/test_pds_script.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -e
-function test_bootstrap {
+
+function test_bootstrap_nvim {
     cd
     wget https://raw.githubusercontent.com/AXGKl/pds/master/setup/pds.sh
     chmod +x 'pds.sh'
-    ./pds.sh
-    ./pds.sh install
+    ./pds.sh && ./pds.sh install
     export PATH="$HOME/pds/bin:$PATH"
     vi --version | grep NVIM
 }
 
 function main {
-    (test_bootstrap)
+    (run test_bootstrap_nvim)
 
 }
 
