9c3465728383cbafb6743661f0ba44f90b81e96a
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-01-08 02:55:03 +0100

    tests: init workflow

---
+++ test/test_all.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+function test_bootstrap {
+    cd
+    wget https://raw.githubusercontent.com/AXGKl/pds/master/setup/pds.sh
+    chmod +x
+    ./pds.sh
+    ./pds.sh install
+    export PATH="$HOME/pds/bin:$PATH"
+    vi --version | grep NVIM
+}
+
+function main {
+    (test_bootstrap)
+
+}
+
+main "$@"
