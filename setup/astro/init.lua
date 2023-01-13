da0a432cd9f7ee52f71f948d8768376292cfd465
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-01-13 10:22:48 +0100

    chore: upgrade mgmt outside astro.

--- setup/astro/init.lua
+++ setup/astro/init.lua
@@ -1,4 +1,8 @@
 UU = require("user.utils") -- allows :lua U.dump(vim.lsp)
+if os.getenv("pds_installing") then
+	return {}
+end
+
 -- TS = require("telescope.builtin")
 local config = {
 	header = {
@@ -16,24 +20,24 @@ local config = {
 		"Type Alt-H for README",
 	},
 	-- Configure AstroNvim updates
-	updater = {
-		remote = "origin", -- remote to use
-		channel = "nightly", -- "stable" or "nightly"
-		version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-		branch = "main", -- branch name (NIGHTLY ONLY)
-		commit = nil, -- commit hash (NIGHTLY ONLY)
-		pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-		skip_prompts = false, -- skip prompts about breaking changes
-		show_changelog = true, -- show the changelog after performing an update
-		auto_reload = false, -- automatically reload and sync packer after a successful update
-		auto_quit = false, -- automatically quit the current session after a successful update
-		-- remotes = { -- easily add new remotes to track
-		--   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
-		--   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
-		--   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
-		-- },
-	},
-
+	-- updater = {
+	-- 	remote = "origin", -- remote to use
+	-- 	channel = "nightly", -- "stable" or "nightly"
+	-- 	version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+	-- 	branch = "main", -- branch name (NIGHTLY ONLY)
+	-- 	commit = nil, -- commit hash (NIGHTLY ONLY)
+	-- 	pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+	-- 	skip_prompts = false, -- skip prompts about breaking changes
+	-- 	show_changelog = true, -- show the changelog after performing an update
+	-- 	auto_reload = false, -- automatically reload and sync packer after a successful update
+	-- 	auto_quit = false, -- automatically quit the current session after a successful update
+	-- 	-- remotes = { -- easily add new remotes to track
+	-- 	--   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
+	-- 	--   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
+	-- 	--   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
+	-- 	-- },
+	-- },
+	--
 	-- Set colorscheme
 	colorscheme = "default_theme",
 	-- set vim options here (vim.<first_key>.<second_key> =  value)
@@ -328,10 +332,6 @@ local config = {
 	-- This function is run last
 	-- good place to configuring augroups/autocommands and custom filetypes
 	polish = function()
-		if os.getenv("pds_installing") then
-			return
-		end
-
 		-- Set key binding
 		-- Set autocommands
 		-- vim.api.nvim_create_augroup("ftplugs", { clear = true })
