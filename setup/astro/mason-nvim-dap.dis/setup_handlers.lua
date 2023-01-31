727e8406ba924e00e308149a4ce48d3dd4ea1ca8
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-01-31 16:04:40 +0100

    chore: mason ttemplate, disabled

--- setup/astro/mason-nvim-dap.dis/setup_handlers.lua
+++ setup/astro/mason-nvim-dap.dis/setup_handlers.lua
@@ -1,26 +1,26 @@
 return {
-  ["mason-nvim-dap"] = {
-    setup_handlers = {
-      python = function(source_name)
-        local dap = require("dap")
-        dap.adapters.python = {
-          type = "executable",
-          command = "/usr/bin/python3",
-          args = {
-            "-m",
-            "debugpy.adapter",
-          },
-        }
+    ['mason-nvim-dap'] = {
+        setup_handlers = {
+            python = function(source_name)
+                local dap = require('dap')
+                dap.adapters.python = {
+                    type = 'executable',
+                    command = '/usr/bin/python3',
+                    args = {
+                        '-m',
+                        'debugpy.adapter',
+                    },
+                }
 
-        dap.configurations.python = {
-          {
-            type = "python",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}", -- This configuration will launch the current file if used.
-          },
-        }
-      end,
+                dap.configurations.python = {
+                    {
+                        type = 'python',
+                        request = 'launch',
+                        name = 'Launch file',
+                        program = '${file}', -- This configuration will launch the current file if used.
+                    },
+                }
+            end,
+        },
     },
-  },
 }
