da2e26efd95ef44ae187224fcf3e73343842e923
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-01-31 16:22:31 +0100

    feat: cmp with emojis

---
+++ setup/astro/cmp/source_priority.lua
@@ -0,0 +1,13 @@
+-- false == disabled
+-- true = 10000
+return {
+    nvim_lsp = 1000,
+    omni = 800,
+    luasnip = 750,
+    emoji = 700,
+    pandoc_references = 700,
+    latex_symbols = 700,
+    calc = 650,
+    path = 500,
+    buffer = 250,
+}
