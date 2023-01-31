61ad5bc9a67d8791db82a33075edcf588ef76801
Gunther Klessinger <gunther.klessinger@axiros.com>
2023-01-31 16:17:32 +0100

    feat: Hooray, multiselect for files

--- setup/astro/plugins/telescope.lua
+++ setup/astro/plugins/telescope.lua
@@ -1,17 +1,17 @@
-local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
 
 local mm = {
-  ["<CR>"] = function(pb)
-    local picker = action_state.get_current_picker(pb)
-    local multi = picker:get_multi_selection()
-    actions.select_default(pb) -- the normal enter behaviour
-    for _, j in pairs(multi) do
-      if j.path ~= nil then
-        vim.cmd(string.format("%s %s", "edit", j.path))
-      end
-    end
-  end,
+    ['<CR>'] = function(pb)
+        local picker = action_state.get_current_picker(pb)
+        local multi = picker:get_multi_selection()
+        actions.select_default(pb) -- the normal enter behaviour
+        for _, j in pairs(multi) do
+            if j.path ~= nil then
+                vim.cmd(string.format('%s %s', 'edit', j.path))
+            end
+        end
+    end,
 }
 
 return { defaults = { mappings = { i = mm, n = mm } } }
